628e4cb262e53c7d97896c3f1d53ce62534daceb
Blet he <412986558@qq.com>
2018-04-19 11:04:16 +0800

    【delete】 delete useless  controller

--- packages/backend/typings/app/service/index.d.ts
+++ packages/backend/typings/app/service/index.d.ts
@@ -1,14 +1,12 @@
 // This file was auto created by egg-ts-helper
 // Do not modify this file!!!!!!!!!
 
-import News from '../../../app/service/News';
 import WxToken from '../../../app/service/WxToken';
 import WxEvent from '../../../app/service/wxEvent';
 import WxMessage from '../../../app/service/wxMessage';
 
 declare module 'egg' {
   interface IService {
-    news: News;
     wxToken: WxToken;
     wxEvent: WxEvent;
     wxMessage: WxMessage;
